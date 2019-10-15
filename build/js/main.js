1325c231b7dd619b32e29b7d360ce045ed76065d
PipZip <37911683+PipZip@users.noreply.github.com>
2019-10-15 17:13:36 +0600

    temp

--- build/js/main.js
+++ build/js/main.js
@@ -41,7 +41,18 @@ $(document).ready(function() {
     });
 
 
-
+    $('.scroll').on('click', function () {
+        var el = $(this);
+        var dest = el.attr('href'); // получаем направление
+        if (dest !== undefined && dest !== '') { // проверяем существование
+            $('body').removeClass('blocked');
+            $('html').animate({
+                    scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+                }, 500 // скорость прокрутки
+            );
+        }
+        return false;
+    });
 
 
 });
